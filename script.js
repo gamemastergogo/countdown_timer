7b0c0fa8853d9d733c8fc077fa84bedabd53730f
sachin <sachinbaste66@gmail.com>
2023-11-27 18:56:15 +0530

    t commit

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const hoursE1 = document.getElementById("hours");
 const minutesE1 = document.getElementById("minutes");
 const secondsE1 = document.getElementById("seconds");
 
-const newYears = "7 Dec 2023";
+const newYears = "8 Dec 2023";
 
 function countdown() {
   const newYearsDate = new Date(newYears);
